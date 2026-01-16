9952d40460df454b2b51879c54a0b4da33d9aff9
Mitchell Victoriano <mitchell.victoriano@gmail.com>
2026-01-15 20:10:40 -0800

    Added basic lume model example

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
-def main():
-    pass
+from lume_model.models import TorchModel
+
+LCLS_CU_INJECTOR_MODEL_PATH = "lcls_cu_injector_ml_model"
+
+
+def main() -> None:
+    model_file_path = LCLS_CU_INJECTOR_MODEL_PATH + "/model_config.yaml"
+    model = TorchModel(model_file_path)
+
+    result = model.evaluate({"QUAD:IN20:425:BACT": -1})
+    print(result)
 
 
 if __name__ == "__main__":
